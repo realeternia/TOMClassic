c52bf3818fe96f856fe4d4d93b9e3f0d476fb486
narlon <narlonzjh@163.com>
2017-06-02 08:43:40 +0800

    disable the key to control page

--- NarlonLib/Control/ViewStack.cs
+++ NarlonLib/Control/ViewStack.cs
@@ -11,5 +11,14 @@ namespace NarlonLib.Control
             if (m.Msg == 0x1328 && !DesignMode) m.Result = (IntPtr)1;
             else base.WndProc(ref m);
         }
+
+        protected override void OnKeyDown(KeyEventArgs ke)
+        {
+            if (ke.KeyCode == Keys.Left || ke.KeyCode == Keys.Right)
+            {
+                ke.Handled = true;
+            }
+            base.OnKeyDown(ke);
+        }
     }
 }
