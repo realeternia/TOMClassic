d390a540a4886e71b7d15a87a53a23eab13cd9e1
narlon <narlon@163.com>
2018-01-28 22:20:04 +0800

    add a card stat program

---
+++ Tool/CardAttrStat/CardAttrStat/CardTypeSub.cs
@@ -0,0 +1,24 @@
+﻿namespace CardAttrStat
+{
+    internal enum CardTypeSub
+    {
+        Devil = 1,
+        Machine = 2,
+        Spirit = 3,
+        Insect = 4,
+        Dragon = 5,
+        Bird = 6,
+        Crawling = 7,
+        Human = 8,
+        Orc = 9,
+        Undead = 10,
+        Beast = 11,
+        Fish = 12,
+        Element = 13,
+        Plant = 14,
+        Goblin = 15,
+        Totem = 16,
+        NormalTower = 34,
+        KingTower = 35,
+    }
+}
---
+++ Tool/CardAttrStat/CardAttrStat/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using ConfigDatas;
+
+namespace CardAttrStat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConfigDatas.ConfigData.LoadData();
+
+            StreamWriter sw = new StreamWriter("./a.txt");
+            for (int i = (int) CardTypeSub.Devil; i <= (int) CardTypeSub.Totem; i++)
+                CheckRace(i, sw);
+            sw.Close();
+        }
+
+        static void CheckRace(int rid, StreamWriter sw)
+        {
+            int tauntCount = 0;
+            int rushCount = 0;
+
+            foreach (var monsterConfig in ConfigData.MonsterDict.Values)
+            {
+                if (monsterConfig.Type != rid)
+                    continue;
+                if (monsterConfig.Skill1 == 55100008 || monsterConfig.Skill2 == 55100008)
+                    tauntCount ++;
+                if (monsterConfig.Skill1 == 55100005 || monsterConfig.Skill2 == 55100005)
+                    rushCount++;
+            }
+            
+            sw.WriteLine("{0}\t{1}", tauntCount, rushCount);
+        }
+    }
+}
