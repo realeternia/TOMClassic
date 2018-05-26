0985a3e3c4180be5a44db36e4deaa3520403538b
narlon <narlon@163.com>
2018-05-26 20:04:08 +0800

    try use all cards on ai round

--- TaleofMonsters2/Controler/Battle/Data/Players/AIStrategy.cs
+++ TaleofMonsters2/Controler/Battle/Data/Players/AIStrategy.cs
@@ -69,89 +69,94 @@ namespace TaleofMonsters.Controler.Battle.Data.Players
 
             int row = BattleManager.Instance.MemMap.RowCount;
             int size = BattleManager.Instance.MemMap.CardSize;
-            bool isLeft = self.IsLeft;
             var rival = self == BattleManager.Instance.PlayerManager.LeftPlayer ? 
                 BattleManager.Instance.PlayerManager.RightPlayer : BattleManager.Instance.PlayerManager.LeftPlayer;
-            
-            self.CardsDesk.SetSelectId(MathTool.GetRandom(self.CardNumber) + 1);
-            if (self.SelectCardId != 0)
+
+            for (int i = 0; i < self.CardNumber; i++)
             {
-                ActiveCard card = self.CardsDesk.GetSelectCard();
-                if (self.CheckUseCard(card, self, rival) != ErrorConfig.Indexer.OK)
+                self.CardsDesk.SetSelectId(i + 1); //逐个判断是否可以使用卡牌
+                if (self.SelectCardId != 0)
+                {
+                    ActiveCard card = self.CardsDesk.GetSelectCard();
+                    if (self.CheckUseCard(card, self, rival) != ErrorConfig.Indexer.OK)
+                        continue;
+
+                    TryUseCard(card, size, row);
+                }
+            }
+       
+        }
+
+        private void TryUseCard(ActiveCard card, int size, int row)
+        {
+            int tar = -1;
+            if (card.CardType == CardTypes.Monster)
+            {
+                Point monPos = GetMonsterPoint(card.CardId, false);
+                self.UseMonster(card, monPos);
+            }
+            else if (card.CardType == CardTypes.Weapon)
+            {
+                for (int i = 0; i < BattleManager.Instance.MonsterQueue.Count; i++)
+                {
+                    LiveMonster monster = BattleManager.Instance.MonsterQueue[i];
+                    if (!monster.IsGhost && monster.IsLeft == self.IsLeft && monster.Weapon == null &&
+                        monster.Hp > monster.RealMaxHp/2)
+                    {
+                        if (!monster.CanAddWeapon()) //建筑无法使用武器
+                            continue;
+
+                        if (tar == -1 || monster.Avatar.Star > BattleManager.Instance.MonsterQueue[tar].Avatar.Star)
+                            tar = i;
+                    }
+                }
+                if (tar == -1)
                     return;
 
-                int tar = -1;
-                if (card.CardType == CardTypes.Weapon)
+                var lm = BattleManager.Instance.MonsterQueue[tar];
+                self.UseWeapon(lm, card);
+            }
+            else if (card.CardType == CardTypes.Spell)
+            {
+                SpellConfig spellConfig = ConfigData.GetSpellConfig(card.CardId);
+                if (BattleTargetManager.IsSpellUnitTarget(spellConfig.Target))
                 {
-                    for (int i = 0; i <BattleManager.Instance.MonsterQueue.Count; i++)
+                    var targetStar = -1;
+                    if (tar >= 0)
+                        targetStar = BattleManager.Instance.MonsterQueue[tar].Avatar.Star;
+                    for (int i = 0; i < BattleManager.Instance.MonsterQueue.Count; i++)
                     {
-                        LiveMonster monster =BattleManager.Instance.MonsterQueue[i];
-                        if (!monster.IsGhost && monster.IsLeft == isLeft && monster.Weapon == null && monster.Hp > monster.RealMaxHp / 2)
+                        LiveMonster monster = BattleManager.Instance.MonsterQueue[i];
+                        if (monster.IsGhost)
+                            continue;
+                        if ((monster.IsLeft != self.IsLeft && spellConfig.Target[1] != 'F') ||
+                            (monster.IsLeft == self.IsLeft && spellConfig.Target[1] != 'E'))
                         {
-                            if (!monster.CanAddWeapon())//建筑无法使用武器
-                                continue;
-
-                            if (tar == -1 || monster.Avatar.Star >BattleManager.Instance.MonsterQueue[tar].Avatar.Star)
+                            if (tar == -1 || monster.Avatar.Star > targetStar)
+                            {
                                 tar = i;
+                                targetStar = monster.Avatar.Star;
+                            }
                         }
                     }
                     if (tar == -1)
                         return;
                 }
-                else if (card.CardType == CardTypes.Spell)
+
+                Point targetPos = Point.Empty;
+                LiveMonster targetMonster = null;
+                if (BattleTargetManager.IsSpellNullTarget(spellConfig.Target))
                 {
-                    SpellConfig spellConfig = ConfigData.GetSpellConfig(card.CardId);
-                    if (BattleTargetManager.IsSpellUnitTarget(spellConfig.Target))
-                    {
-                        var targetStar = -1;
-                        if (tar >= 0)
-                            targetStar = BattleManager.Instance.MonsterQueue[tar].Avatar.Star;
-                        for (int i = 0; i <BattleManager.Instance.MonsterQueue.Count; i++)
-                        {
-                            LiveMonster monster =BattleManager.Instance.MonsterQueue[i];
-                            if(monster.IsGhost)
-                                continue;
-                            if ((monster.IsLeft != isLeft && spellConfig.Target[1] != 'F') || (monster.IsLeft == isLeft && spellConfig.Target[1] != 'E'))
-                            {
-                                if (tar == -1 || monster.Avatar.Star > targetStar)
-                                {
-                                    tar = i;
-                                    targetStar = monster.Avatar.Star;
-                                }
-                            }
-                        }
-                        if (tar == -1)
-                            return;
-                    }
+                    targetPos = new Point(self.IsLeft ? MathTool.GetRandom(200, 300) : MathTool.GetRandom(600, 700),
+                        MathTool.GetRandom(size * 3 / 10, row * size - size * 3 / 10));
+                }
+                else if (BattleTargetManager.IsSpellUnitTarget(spellConfig.Target))
+                {
+                    targetMonster = BattleManager.Instance.MonsterQueue[tar];
+                    targetPos = targetMonster.CenterPosition;
                 }
 
-                if (card.CardType == CardTypes.Monster)
-                {
-                    Point monPos = GetMonsterPoint(card.CardId, false);
-                    self.UseMonster(card, monPos);
-                }
-                else if (card.CardType == CardTypes.Weapon)
-                {
-                    var lm =BattleManager.Instance.MonsterQueue[tar];
-                    self.UseWeapon(lm, card);
-                }
-                else if (card.CardType == CardTypes.Spell)
-                {
-                    SpellConfig spellConfig = ConfigData.GetSpellConfig(card.CardId);
-                    Point targetPos = Point.Empty;
-                    LiveMonster targetMonster = null;
-                    if (BattleTargetManager.IsSpellNullTarget(spellConfig.Target))
-                    {
-                        targetPos = new Point(isLeft ? MathTool.GetRandom(200, 300) : MathTool.GetRandom(600, 700), MathTool.GetRandom(size * 3 / 10, row * size - size * 3 / 10));
-                    }
-                    else if (BattleTargetManager.IsSpellUnitTarget(spellConfig.Target))
-                    {
-                        targetMonster = BattleManager.Instance.MonsterQueue[tar];
-                        targetPos = targetMonster.CenterPosition;
-                    }
-
-                    self.DoSpell(targetMonster, card, targetPos);
-                }
+                self.DoSpell(targetMonster, card, targetPos);
             }
         }
 
