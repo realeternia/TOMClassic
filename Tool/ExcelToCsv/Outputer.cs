16a88bafc6bedbd884185a667659456a2446055f
mosac <narlon@163.com>
2018-07-29 15:33:59 +0800

    change a parms name for relic event

--- Tool/ExcelToCsv/Outputer.cs
+++ Tool/ExcelToCsv/Outputer.cs
@@ -219,7 +219,7 @@ namespace ExcelToCsv
                 {
                     result = datas[i].ToString() == ""
                                   ? "null"
-                                  : string.Format("delegate(IPlayer p, IRelic r, int cid, int type, IMonster m,ref bool result){{{0}}}", datas[i]);
+                                  : string.Format("delegate(IPlayer p, IRelic r, int cid, int type, IMonster t,ref bool result){{{0}}}", datas[i]);
                 }
                 else if (typeStr == "EquipMonsterPickDelegate")
                 {
